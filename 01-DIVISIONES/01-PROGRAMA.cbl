e3437bdd031503b8e3e99de03affc8b431c1a688
Rayo97 <47052438+Rayo97@users.noreply.github.com>
2025-06-12 17:15:24 -0600

    Add files via upload

---
+++ 01-DIVISIONES/01-PROGRAMA.cbl
@@ -0,0 +1,37 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       PROGRAM-ID. 01-PROGRAMA.
+      *-NOMBRE DEL PROGRAMA
+       AUTHOR.        RAY MARTINEZ
+       INSTALLATION.  01-DIVISIONES
+       DATE-WRITTEN.  9/6/2024
+       DATE-COMPILED. 9/6/2024
+       REMARKS.       PRIMER PROGRAMA
+       ENVIRONMENT DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       CONFIGURATION SECTION.
+      *-----------------------
+       INPUT-OUTPUT SECTION.
+      *-----------------------
+       DATA DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       FILE SECTION.
+      *-----------------------
+       WORKING-STORAGE SECTION.
+      *-----------------------
+       PROCEDURE DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       MAIN-PROCEDURE.
+      **
+      * The main procedure of the program
+      **
+            DISPLAY "Hello world"
+            STOP RUN.
+      ** add other procedures here
+       END PROGRAM 01-PROGRAMA.
